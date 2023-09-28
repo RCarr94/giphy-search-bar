301ec0362cadbdbc6ed787eb874d3cbc3732fea6
Ryan Carr <ryan.g.carr@hotmail.com>
2023-09-28 10:20:02 -0400

    cleaned up UI, added useEffect to fill initial page, used async function for getGifs

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
-import { useState } from 'react';
+import { useState, useEffect} from 'react';
 import './App.css';
 import SearchBar from './components/SearchBar';
 import GifList from './components/GifList';
 
 export default function App() {
-  const [searchImgs, setSearchImgs] = useState('');
+  const [searchImgs, setSearchImgs] = useState('hello');
   const [gifs, setGifs] = useState([]);
+
+  useEffect(() => {
+    getGifs(searchImgs)
+  },[]);
 
   const searchOptions = {
     key: '93sHYga5MbpfCx5UT4WjVAQXXqK4XP9Y',
     api: 'https://api.giphy.com/v1/gifs/search'
   };
 
-function getGifs(searchImgs) {
+async function getGifs(searchImgs) {
   const url = `${searchOptions.api}?api_key=${searchOptions.key}&q=${searchImgs}`;
 
-  fetch(url)
-    .then((res) => res.json())
-    .then((res) => {
-      setGifs(res.data);
-    })
+    const res = await fetch(url);
+    const data = await res.json();
+    setGifs(data.data);
+    setSearchImgs('');
 }
 
   return (
